d60b4c01445b3fe9d709f8c2b4ecadefcbe29267
James Pharaoh <james@pharaoh.uk>
2016-04-27 10:34:09 +0000

    queue debugging and fixes, time handling improvements, basic import control

---
+++ forms/FieldsProvider.java
@@ -0,0 +1,19 @@
+package wbs.console.forms;
+
+import wbs.framework.record.Record;
+
+public
+interface FieldsProvider<
+	ObjectType extends Record<ObjectType>,
+	ParentType extends Record<ParentType>
+> {
+
+	FormFieldSet getStaticFields ();
+
+	FormFieldSet getFieldsForParent (
+			ParentType parent);
+
+	FormFieldSet getFieldsForObject (
+			ObjectType object);
+
+}
---
+++ forms/StaticFieldsProvider.java
@@ -0,0 +1,59 @@
+package wbs.console.forms;
+
+import lombok.NonNull;
+
+import wbs.framework.application.annotations.PrototypeComponent;
+import wbs.framework.record.Record;
+
+@PrototypeComponent ("staticFieldsProvider")
+public
+class StaticFieldsProvider<
+	ObjectType extends Record<ObjectType>,
+	ParentType extends Record<ParentType>
+>
+	implements FieldsProvider<ObjectType,ParentType> {
+
+	// state
+
+	FormFieldSet formFields;
+
+	// implementation
+
+	@Override
+	public
+	FormFieldSet getFieldsForObject (
+			@NonNull ObjectType object) {
+
+		return getStaticFields ();
+
+	}
+
+	@Override
+	public
+	FormFieldSet getFieldsForParent (
+			@NonNull ParentType parent) {
+
+		return getStaticFields ();
+
+	}
+
+	@Override
+	public
+	FormFieldSet getStaticFields () {
+
+		return formFields;
+
+	}
+
+	public
+	StaticFieldsProvider<ObjectType,ParentType> setFields (
+			@NonNull FormFieldSet fields) {
+
+		formFields =
+			fields;
+
+		return this;
+
+	}
+
+}
